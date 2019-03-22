6f12c1fe289f577074bd2ccebbabfcf771ee9b80
Wolfgang Ebner <wolfgang.p.ebner@gmail.com>
2019-03-22 13:22:13 +0000

    write prometheus metrics

--- common.sh
+++ common.sh
@@ -23,3 +23,47 @@ function get_oc_selector() {
 function get_nodes() {
   oc get nodes --no-headers -o custom-columns=name:.metadata.name
 }
+
+function prom_start() {
+  PROM_START_TIME=$(date +%s%03N)
+}
+
+function _update_begin() {
+  PROM_FILE=$1 
+
+  if [[ -e "$PROM_FILE" ]]; then
+   cp "$PROM_FILE" "${PROM_FILE}.temp"
+  else 
+   touch "${PROM_FILE}.temp"
+  fi
+}
+
+function _update_value() {
+  key=$1
+  value=$2
+
+  grep -q "^$key" ${PROM_FILE}.temp && 
+    sed "s#\($key\).*#\1 $value#" -i ${PROM_FILE}.temp ||
+    echo "${key} $value" >> ${PROM_FILE}.temp
+}
+
+function _update_commit() {
+  chmod +r ${PROM_FILE}.temp
+  mv ${PROM_FILE}.temp ${PROM_FILE}
+}
+
+function prom_end() {
+  exitcode=$?
+  PROM_NAME=$1
+  PROM_LABELS=$2
+  end=$(date +%s%03N)
+  runtime=$((end-PROM_START_TIME))
+
+  _update_begin ${BACKUP_TARGET}/metrics/${PROM_NAME}.prom
+  _update_value "${PROM_NAME}_exitcode{$PROM_LABELS}" "$exitcode"
+  _update_value "${PROM_NAME}_runtime_ms{$PROM_LABELS}" "$runtime"
+  _update_commit
+  
+  return $exitcode
+}
+
